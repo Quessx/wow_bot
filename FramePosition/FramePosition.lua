2f9e43985c3d00979d78cd897dfbc2a973934db2
brezovskii <berezovskii.d@introvert.bz>
2024-04-17 03:26:11 +0300

    add /reload button & frame

---
+++ FramePosition/FramePosition.lua
@@ -0,0 +1,14 @@
+print('start frame');
+
+local f = CreateFrame("Frame", "DragFrame2", UIParent);
+local btn = CreateFrame("Button", nil, UIParent, "UIPanelButtonTemplate");
+btn:SetPoint("BOTTOMLEFT", UIParent, 30, 350);
+btn:SetSize(100, 40);
+btn:SetText("Click me");
+btn:SetScript("OnClick", function(self, button, down);
+	print("Pressed", button, down and "down" or "up");
+	if not down then
+		return ReloadUI();
+	end;
+end);
+btn:RegisterForClicks("AnyDown", "AnyUp");
